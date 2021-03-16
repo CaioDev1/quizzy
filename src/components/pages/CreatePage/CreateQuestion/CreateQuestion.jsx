745f8d34197ef84ab56dc9adcaffd7fcf6796aab
Caio Felipe <imcaiofelipe@gmail.com>
2021-03-16 16:48:25 -0300

    last commit

--- src/components/pages/CreatePage/CreateQuestion/CreateQuestion.jsx
+++ src/components/pages/CreatePage/CreateQuestion/CreateQuestion.jsx
@@ -132,7 +132,11 @@ function CreateQuestion({setQuestionList, index}) {
                     )
                 })}
                 {question.alternatives.length <= 3 &&
-                    <button className='add-alternative-button' onClick={handleNewAlternative}>+</button>
+                    <button className='add-alternative-button' onClick={handleNewAlternative}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+                        </svg>
+                    </button>
                 }
             </div>
         </div>
--- src/views/CreatePage/CreatePage.jsx
+++ src/views/CreatePage/CreatePage.jsx
@@ -129,7 +129,11 @@ function CreatePage() {
                     {questionList.map((item, i) => {
                         return <CreateQuestion setQuestionList={setQuestionList} index={i} key={i} />
                     })}
-                    <button id='add-question' onClick={addNewQuestion}>+</button>
+                    <button id='add-question' onClick={addNewQuestion}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z"/>
+                        </svg>
+                    </button>
                 </div>
                 <form id='create-question-menu' onSubmit={handleNewQuizzRequest}>
                     <div id='question-menu-rows'>
